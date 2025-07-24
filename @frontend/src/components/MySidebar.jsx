a3c6eba0e04e884fbb8f2dab28f22a15a4e9307a
SibyB <siby.pixous@gmail.com>
2025-07-24 17:34:03 +0530

    navigate
    navigate

--- @frontend/src/components/MySidebar.jsx
+++ @frontend/src/components/MySidebar.jsx
@@ -1,68 +1,3 @@
-// import React from 'react';
-// import { ProSidebarProvider, Sidebar, Menu, MenuItem , SubMenu } from 'react-pro-sidebar';
-// import { Link } from 'react-router-dom';
-
-
-// const MySidebar = () => {
-//   return (
-
-//     <ProSidebarProvider>
-//   <Sidebar style={{
-//       position: 'fixed',
-//       height: '100vh',
-//       overflowY: 'auto',
-//       // backgroundColor:'green' 
-//       // ensures scroll if content overflows
-      
-//     }}>
-//     <Menu menuItemStyles={{
-//       button: { "&.active": { backgroundColor: '#13395e', color: '#b6c8d9' } }
-//     }}>
-//       <SubMenu label="Dashboard">
-//         <MenuItem>Overview</MenuItem>
-//         <MenuItem>Stats</MenuItem>
-//       </SubMenu>
-//       <SubMenu label="Employees">
-//         <MenuItem>List</MenuItem>
-//         <MenuItem>Add New</MenuItem>
-//       </SubMenu>
-//       <SubMenu label="Departments">
-//         <MenuItem>HR</MenuItem>
-//         <MenuItem>Finance</MenuItem>
-//       </SubMenu>
-//       <SubMenu label="Leaves">
-//         <MenuItem>Apply</MenuItem>
-//         <MenuItem>History</MenuItem>
-//       </SubMenu>
-//       <SubMenu label="Salary">
-//         <MenuItem>Pay Slips</MenuItem>
-//         <MenuItem>Settings</MenuItem>
-//       </SubMenu>
-//       <SubMenu label="Attendance">
-//         <MenuItem>Daily</MenuItem>
-//         <MenuItem>Report</MenuItem>
-//       </SubMenu>
-//       <SubMenu label="Settings">
-//         <MenuItem>Profile</MenuItem>
-//         <MenuItem>Security</MenuItem>
-//       </SubMenu>
-//     </Menu>
-//   </Sidebar>
-// </ProSidebarProvider>
-//   );
-// };
-
-// export default MySidebar;
-
-
-
-
-
-
-
-
-
-
 import React from 'react';
 import { ProSidebarProvider, Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar';
 import { Tooltip } from 'react-tooltip';
@@ -77,7 +12,7 @@ const SidebarContent = () => {
 
   const subMenus = [
     { label: 'Dashboard', icon: MdDashboard, items: ['Overview','Stats'], to: ['overview','stats'] },
-    { label: 'Employees', icon: IoPeopleSharp, items: ['List','Add New'], to: ['employees','employees/add'] },
+    { label: 'Employees', icon: IoPeopleSharp, items: ['List','Add New'], to: ['employee','employee/add'] },
     { label: 'Department',icon: IoPeopleSharp, items:['List','Add New'], to:['department','department']},
     { label: 'Leaves',icon: IoPeopleSharp, items:['List','Add New'], to:['department','department']},
     { label: 'Salary',icon: IoPeopleSharp, items:['List','Add New'], to:['department','department']},
